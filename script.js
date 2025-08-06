4fb9d68301f26256272de75b8f7c68df440cf0b7
yoda0927 <yohann.remuszka@gmail.com>
2025-08-06 10:42:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ function login(){
     var nome = $('#senha').val()
     console.log(nome,senha)    
 }
+
+function showPassword(){
+    var input_senha = document.querySelector('#senha')
+    var img_eye = document.querySelector('#eye')
+
+    if(input_senha.getAttribute('type') === 'password'){
+        input_senha.setAttribute('type', 'text')
+        img_eye.setAttribute('src', './images/eye-not.png')
+    }else{
+        input_senha.setAttribute('type', 'password')
+        img_eye.setAttribute('src', './images/eye.png')
+    }
+}
